5a9df568d52ce6182c37414219b9eb6dc36f6667
LE FLOCH Tanguy <tanguy.lefloch@soprasteria.com>
2019-05-30 23:40:19 +0200

    Cleaning and optimisation of the fuzzy choice

--- fuzzy_tools_plugin_id/custom-recipes/fuzzy_choice/recipe.py
+++ fuzzy_tools_plugin_id/custom-recipes/fuzzy_choice/recipe.py
@@ -1,78 +1,49 @@
-# Code for custom code recipe fuzzy_filter (imported from a Python recipe)
-
-# To finish creating your custom recipe from your original PySpark recipe, you need to:
-#  - Declare the input and output roles in recipe.json
-#  - Replace the dataset names by roles access in your code
-#  - Declare, if any, the params of your custom recipe in recipe.json
-#  - Replace the hardcoded params values by acccess to the configuration map
-
-# See sample code below for how to do that.
-# The code of your original recipe is included afterwards for convenience.
-# Please also see the "recipe.json" file for more information.
-
-# import the classes for accessing DSS objects from the recipe
 import dataiku
-# Import the helpers for custom recipes
 from dataiku.customrecipe import *
 
-# Inputs and outputs are defined by roles. In the recipe's I/O tab, the user can associate one
-# or more dataset to each input and output role.
-# Roles need to be defined in recipe.json, in the inputRoles and outputRoles fields.
+from functools import partial
+from fuzzywuzzy import fuzz, process
+import pandas as pd
+import string
+from unidecode import unidecode
 
-# To  retrieve the datasets of an input role named 'input_A' as an array of dataset names:
+""" This recipes does the following :
+1) Loads the datasets & the parameters
+2) Prepares the functions to be used on the data
+3) Apply get_choices_serie_vals over the whole dataframe line by line
+4) Returns the result by merging the resulting dataframe to the original one
+"""
+
+# Fetching dataiku datasets
 input_dataset_name = get_input_names_for_role('input_dataset')[0]
-# The dataset objects themselves can then be created like this:
 input_dataset = dataiku.Dataset(input_dataset_name)
 
 values_dataset_name = get_input_names_for_role('values_dataset')[0]
 values_dataset = dataiku.Dataset(values_dataset_name)
 
-# For outputs, the process is the same:
 output_dataset_name = get_output_names_for_role('output_dataset')[0]
 output_dataset = dataiku.Dataset(output_dataset_name)
-
-
-# The configuration consists of the parameters set up by the user in the recipe Settings tab.
-
-# Parameters must be added to the recipe.json file so that DSS can prompt the user for values in
-# the Settings tab of the recipe. The field "params" holds a list of all the params for wich the
-# user will be prompted for values.
-
-# The configuration is simply a map of parameters, and retrieving the value of one of them is simply:
-parameters = get_recipe_config()
-
-# For optional parameters, you should provide a default value in case the parameter is not present:
-# my_variable = get_recipe_config().get('parameter_name', None)
-
-# Note about typing:
-# The configuration of the recipe is passed through a JSON object
-# As such, INT parameters of the recipe are received in the get_recipe_config() dict as a Python float.
-# If you absolutely require a Python int, use int(get_recipe_config()["my_int_param"])
-
-
-#############################
-# Your original recipe
-#############################
-
-# -*- coding: utf-8 -*-
-import pandas as pd
-from fuzzywuzzy import fuzz, process
-from functools import partial
-import string
-from unidecode import unidecode
 
 # Read recipe inputs
 data_df = input_dataset.get_dataframe()
 values_df = values_dataset.get_dataframe()
 
 # Reading ui parameters
+parameters = get_recipe_config()
+
+# Mandatory params
 target_col_name = parameters["target_col"]
-values = parameters["values"].split(',')
-values_colname = parameters["values_colname"]
 nb_choices = int(parameters["nb_choices"])
 thresh = parameters["thresh"]
 ratio_type = parameters["ratio_type"]
 ratios = parameters["ratios"]
+
+# Optional params : either values from a list or from a column
+values = parameters.get("values", [])
+values_colname = parameters.get("values_colname", '')
+
+if values:
+    values = values.split(',')
 
 if values_colname:
     values_series = values_df[values_colname]
@@ -80,16 +51,17 @@ if values_colname:
         values += values_series.tolist()
 
 if nb_choices > len(values):
-    raise ValueError("More number of choices than provided values.")
+    raise ValueError("Bigger number of choices than count of provided values.")
 
 # fetching the scorer to use from the fuzzywuzzy module
 ratio_fct = getattr(fuzz, ratio_type)
 
+# Python 2 fun
 printable = set(string.printable)
 
 
 def clear_text(text):
-    """ Tries to remove accents but keep the underlying letter using unidecode. 
+    """ Tries to remove accents but keep the underlying letter using unidecode.
         If it fails, just removes the non-ascii chars """
     try:
         return unidecode(text)
@@ -98,28 +70,47 @@ def clear_text(text):
         return filter(lambda x: x in printable, text)
 
 
-def get_choices(text, vals, nb_choices=1):
+# Actuel use of fuzzywuzzy
+def get_choices(text, choices, number_of_choices=1):
     """ Returns a list of tuples with (choice, score) """
-    return process.extract(clear_text(text), vals, limit=nb_choices, scorer=ratio_fct)
+    return process.extract(clear_text(text), choices, limit=number_of_choices, scorer=ratio_fct)
 
 
 choice_key = "choice_%d"
 choice_ratio_key = choice_key + "_ratio"
 
 
-def get_choices_serie(text, vals, nb_choices=1, ratios=False):
-    res_dict = {}
-    for i, res in enumerate(get_choices(text=text, vals=vals, nb_choices=nb_choices)):
-        pass_thresh = res[1] > thresh
-        res_dict[choice_key % i] = res[0] if pass_thresh else ''
-        if ratios:
-            res_dict[choice_ratio_key % i] = res[1] if pass_thresh else ''
+def get_choices_serie(text, choices, base_res_dict, number_of_choices=1, return_ratios=False):
+    """ Returns a series with an entry for each choice (an another for ratios if expected)"""
+
+    res_dict = base_res_dict.copy()
+
+    # As long as we get viable choices (above the thresh) we keep filling the dict
+    for i, res in enumerate(get_choices(text=text, choices=choices, number_of_choices=number_of_choices)):
+        pass_thresh_cond = res[1] > thresh
+        if pass_thresh_cond:
+            res_dict[choice_key % i] = res[0]
+
+            if return_ratios:
+                res_dict[choice_ratio_key % i] = res[1]
+        else:
+            break
 
     return pd.Series(res_dict)
 
 
+# Preparing the values and the base dict used for the line by line result
 values = list(set([clear_text(val) for val in values]))
-get_choices_serie_vals = partial(get_choices_serie, vals=values, nb_choices=nb_choices, ratios=ratios)
+pre_filled_dict = {choice_key % i: '' for i in range(nb_choices)}
+if ratios:
+    pre_filled_dict.update({choice_ratio_key % i: '' for i in range(nb_choices)})
+
+# Preparing the function used on each line
+get_choices_serie_vals = partial(get_choices_serie,
+                                 choices=values,
+                                 base_res_dict=pre_filled_dict,
+                                 number_of_choices=nb_choices,
+                                 return_ratios=ratios)
 
 data = data_df[target_col_name].astype(str).fillna('')
 result_df = data_df.merge(data.apply(get_choices_serie_vals), left_index=True, right_index=True)
